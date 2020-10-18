7051c9260710258662f9a4813624bb2784a39711
Kazuki Murahama <kazuki.murahama@gmail.com>
2020-10-18 22:49:07 +0900

    added a first storycontent

---
+++ src/stories/index.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Meta } from '@storybook/react/types-6-0';
+
+export const Component: React.FC = () => <div>Storytest</div>;
+
+export default {
+  title: 'Index',
+} as Meta;
