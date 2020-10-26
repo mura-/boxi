e63dbccb41e35bbc538499df710604990f92de6a
Kazuki Murahama <kazuki.murahama@gmail.com>
2020-10-27 06:47:09 +0900

    added X and Y margins / paddings

--- src/mixins/margins.ts
+++ src/mixins/margins.ts
@@ -8,11 +8,15 @@ export interface MarginsProps {
   mr?: CSSProperties['marginRight'];
   mb?: CSSProperties['marginBottom'];
   ml?: CSSProperties['marginLeft'];
+  my?: CSSProperties['margin'];
+  mx?: CSSProperties['margin'];
   margin?: CSSProperties['margin'];
   marginTop?: CSSProperties['marginTop'];
   marginRight?: CSSProperties['marginRight'];
   marginBottom?: CSSProperties['marginBottom'];
   marginLeft?: CSSProperties['marginLeft'];
+  marginY?: CSSProperties['margin'];
+  marginX?: CSSProperties['margin'];
 }
 
 export const margins = ({
@@ -21,19 +25,30 @@ export const margins = ({
   mr,
   mb,
   ml,
-
+  my,
+  mx,
   margin,
   marginTop,
   marginRight,
   marginBottom,
   marginLeft,
+  marginY,
+  marginX,
 }: MarginsProps): SerializedStyles => css`
   ${m && `margin: ${px(m)}`};
+  ${my && `margin-top: ${px(my)}`};
+  ${my && `margin-bottom: ${px(my)}`};
+  ${mx && `margin-left: ${px(mx)}`};
+  ${mx && `margin-right: ${px(mx)}`};
   ${mt && `margin-top: ${px(mt)}`};
   ${mr && `margin-right: ${px(mr)}`};
   ${mb && `margin-bottom: ${px(mb)}`};
   ${ml && `margin-left: ${px(ml)}`};
   ${margin && `margin: ${px(margin)}`};
+  ${marginY && `margin-top: ${px(marginY)}`};
+  ${marginY && `margin-bottom: ${px(marginY)}`};
+  ${marginX && `margin-left: ${px(marginX)}`};
+  ${marginX && `margin-right: ${px(marginX)}`};
   ${marginTop && `margin-top: ${px(marginTop)}`};
   ${marginRight && `margin-right: ${px(marginRight)}`};
   ${marginBottom && `margin-bottom: ${px(marginBottom)}`};
--- src/mixins/paddings.ts
+++ src/mixins/paddings.ts
@@ -1,6 +1,6 @@
 import { css, SerializedStyles } from '@emotion/core';
 import { CSSProperties } from 'react';
-import { px } from '../helpers';
+import { px as pxh } from '../helpers';
 
 export interface PaddingsProps {
   p?: CSSProperties['padding'];
@@ -8,11 +8,15 @@ export interface PaddingsProps {
   pr?: CSSProperties['paddingRight'];
   pb?: CSSProperties['paddingBottom'];
   pl?: CSSProperties['paddingLeft'];
+  py?: CSSProperties['padding'];
+  px?: CSSProperties['padding'];
   padding?: CSSProperties['padding'];
   paddingTop?: CSSProperties['paddingTop'];
   paddingRight?: CSSProperties['paddingRight'];
   paddingBottom?: CSSProperties['paddingBottom'];
   paddingLeft?: CSSProperties['paddingLeft'];
+  paddingY?: CSSProperties['padding'];
+  paddingX?: CSSProperties['padding'];
 }
 
 export const paddings = ({
@@ -21,20 +25,32 @@ export const paddings = ({
   pr,
   pb,
   pl,
+  py,
+  px,
   padding,
   paddingTop,
   paddingRight,
   paddingBottom,
   paddingLeft,
+  paddingY,
+  paddingX,
 }: PaddingsProps): SerializedStyles => css`
-  ${p && `padding: ${px(p)}`};
-  ${pt && `padding-top: ${px(pt)}`};
-  ${pr && `padding-right: ${px(pr)}`};
-  ${pb && `padding-bottom: ${px(pb)}`};
-  ${pl && `padding-left: ${px(pl)}`};
-  ${padding && `padding: ${px(padding)}`};
-  ${paddingTop && `padding-top: ${px(paddingTop)}`};
-  ${paddingRight && `padding-right: ${px(paddingRight)}`};
-  ${paddingBottom && `padding-bottom: ${px(paddingBottom)}`};
-  ${paddingLeft && `padding-left: ${px(paddingLeft)}`};
+  ${p && `padding: ${pxh(p)}`};
+  ${py && `margin-top: ${pxh(py)}`};
+  ${py && `margin-bottom: ${pxh(py)}`};
+  ${px && `margin-left: ${pxh(px)}`};
+  ${px && `margin-right: ${pxh(px)}`};
+  ${pt && `padding-top: ${pxh(pt)}`};
+  ${pr && `padding-right: ${pxh(pr)}`};
+  ${pb && `padding-bottom: ${pxh(pb)}`};
+  ${pl && `padding-left: ${pxh(pl)}`};
+  ${padding && `padding: ${pxh(padding)}`};
+  ${paddingY && `margin-top: ${pxh(paddingY)}`};
+  ${paddingY && `margin-bottom: ${pxh(paddingY)}`};
+  ${paddingX && `margin-left: ${pxh(paddingX)}`};
+  ${paddingX && `margin-right: ${pxh(paddingX)}`};
+  ${paddingTop && `padding-top: ${pxh(paddingTop)}`};
+  ${paddingRight && `padding-right: ${pxh(paddingRight)}`};
+  ${paddingBottom && `padding-bottom: ${pxh(paddingBottom)}`};
+  ${paddingLeft && `padding-left: ${pxh(paddingLeft)}`};
 `;
