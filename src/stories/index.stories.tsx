49b7c6f38bda73db59e2ea7ca63929c5cf14d19b
Kazuki Murahama <kazuki.murahama@gmail.com>
2020-11-09 07:41:15 +0900

    tweaks stories

--- src/stories/index.stories.tsx
+++ src/stories/index.stories.tsx
@@ -5,41 +5,41 @@ import Box from '../index';
 export const Component: React.FC = () => <Box backgroundColor="red">Storytest</Box>;
 export const ComponentWithFlex: React.FC = () => (
   <Box display="flex" justifyContent="space-between">
-    <Box>hoge</Box>
-    <Box>hoge</Box>
+    <Box>Children</Box>
+    <Box>Children</Box>
   </Box>
 );
 
 export const ComponentWithSizing: React.FC = () => (
   <Box display="flex" alignItems="center" justifyContent="space-between" height="300px">
-    <Box>hoge</Box>
-    <Box>hoge</Box>
+    <Box>Children</Box>
+    <Box>Children</Box>
   </Box>
 );
 
 export const ComponentWithAlign: React.FC = () => (
   <Box width="100%" textAlign="center">
-    <Box>hoge</Box>
+    <Box>Children</Box>
   </Box>
 );
 
 export const ComponentWithInline: React.FC = () => (
   <Box width="100%">
     <Box display="inline" backgroundColor="yellow">
-      hoge
+      Children
     </Box>
     <Box display="inline" backgroundColor="red">
-      hoge
+      Children
     </Box>
     <Box display="inline" backgroundColor="blue">
-      hoge
+      Children
     </Box>
   </Box>
 );
 
 export const ComponentWithBorder: React.FC = () => (
   <Box borderTop="1px" borderColor="red">
-    hoge
+    Children
   </Box>
 );
 
