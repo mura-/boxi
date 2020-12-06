17e9ef85fae0b68ae58fe32e4b15ab96640fc5f5
Kazuki Murahama <kazuki.murahama@gmail.com>
2020-12-06 11:11:28 +0900

    added media query functions

---
+++ src/mixins/breakpoints.ts
@@ -0,0 +1,57 @@
+import { css, SerializedStyles } from '@emotion/core';
+import { CSSProperties } from 'react';
+import { px } from '../helpers';
+// The breakpoint **start** at this value.
+// For instance with the first breakpoint xs: [xs, sm[.
+const values = {
+  xs: 0,
+  sm: 600,
+  md: 960,
+  lg: 1280,
+  xl: 1920,
+};
+
+export type BreakpointsValues<CSSProperty extends string> = {
+  xs?: CSSProperty;
+  sm?: CSSProperty;
+  md?: CSSProperty;
+  lg?: CSSProperty;
+  xl?: CSSProperty;
+};
+
+const defaultBreakpoints = {
+  // Sorted ASC by size. That's important.
+  // It can't be configured as it's used statically for propTypes.
+  keys: (['xs', 'sm', 'md', 'lg', 'xl'] as unknown) as (keyof typeof values)[],
+  up: (key: keyof typeof values) => `@media (min-width:${values[key]}px)`,
+};
+
+export const createMediaQuery = ({
+  name,
+  value,
+  key,
+}: {
+  name: string;
+  value: string;
+  key: keyof typeof values;
+}): string => {
+  return `
+    @media (min-width: ${values[key]}px) {
+      ${name}: ${value};
+    }
+  `;
+};
+
+export const restyle = <CSSProperty extends string>({
+  name,
+  breakpoints,
+}: {
+  name: string;
+  breakpoints: BreakpointsValues<CSSProperty> | string;
+}): string => {
+  if (typeof breakpoints === 'string') return `${name}: ${breakpoints}`;
+  return defaultBreakpoints.keys
+    .filter((k) => !!breakpoints[k])
+    .map((k) => createMediaQuery({ name, key: k, value: breakpoints[k] }))
+    .join('\n');
+};
