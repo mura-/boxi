f2382b17f466cccd6e32ded73a0efd2b826039d5
Kazuki Murahama <kazuki.murahama@gmail.com>
2020-12-11 07:23:18 +0900

    using breakpoints in shadows

--- src/mixins/shadows.ts
+++ src/mixins/shadows.ts
@@ -1,10 +1,11 @@
 import { css, SerializedStyles } from '@emotion/core';
 import { CSSProperties } from 'react';
+import { BreakpointsValues, restyle } from '../mixins/breakpoints';
 
 export interface ShadowsProps {
-  boxShadow?: CSSProperties['boxShadow'];
+  boxShadow?: CSSProperties['boxShadow'] | BreakpointsValues<CSSProperties['boxShadow']>;
 }
 
 export const shadows = ({ boxShadow }: ShadowsProps): SerializedStyles => css`
-  box-shadow: ${boxShadow};
+  ${boxShadow && restyle({ name: 'box-shadow', breakpoints: boxShadow })};
 `;
