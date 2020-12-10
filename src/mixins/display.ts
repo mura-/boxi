d101bb7276325672c52b6129672ce6c43436d1de
Kazuki Murahama <kazuki.murahama@gmail.com>
2020-12-10 09:04:49 +0900

    using breakpoints in display

--- src/mixins/display.ts
+++ src/mixins/display.ts
@@ -1,12 +1,13 @@
 import { css, SerializedStyles } from '@emotion/core';
 import { CSSProperties } from 'react';
+import { BreakpointsValues, restyle } from '../mixins/breakpoints';
 
 export interface DisplayProps {
-  display?: CSSProperties['display'];
-  overflow?: CSSProperties['overflow'];
-  textOverflow?: CSSProperties['textOverflow'];
-  visibility?: CSSProperties['visibility'];
-  whiteSpace?: CSSProperties['whiteSpace'];
+  display?: CSSProperties['display'] | BreakpointsValues<CSSProperties['display']>;
+  overflow?: CSSProperties['overflow'] | BreakpointsValues<CSSProperties['overflow']>;
+  textOverflow?: CSSProperties['textOverflow'] | BreakpointsValues<CSSProperties['textOverflow']>;
+  visibility?: CSSProperties['visibility'] | BreakpointsValues<CSSProperties['visibility']>;
+  whiteSpace?: CSSProperties['whiteSpace'] | BreakpointsValues<CSSProperties['whiteSpace']>;
 }
 
 export const display = ({
@@ -16,9 +17,9 @@ export const display = ({
   visibility,
   whiteSpace,
 }: DisplayProps): SerializedStyles => css`
-  display: ${display};
-  overflow: ${overflow};
-  text-overflow: ${textOverflow};
-  visibility: ${visibility};
-  white-space: ${whiteSpace};
+  ${display && restyle({ name: 'display', breakpoints: display })};
+  ${overflow && restyle({ name: 'overflow', breakpoints: overflow })};
+  ${textOverflow && restyle({ name: 'text-overflow', breakpoints: textOverflow })};
+  ${visibility && restyle({ name: 'visiblity', breakpoints: visibility })};
+  ${whiteSpace && restyle({ name: 'white-space', breakpoints: whiteSpace })};
 `;
